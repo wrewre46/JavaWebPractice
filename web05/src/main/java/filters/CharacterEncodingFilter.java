f2df1c2a78fd836ec3da06f09d8152e817234653
wrewre46 <wrewre46@naver.com>
2022-01-14 21:08:21 +0900

    web05-view component

---
+++ web05/src/main/java/filters/CharacterEncodingFilter.java
@@ -0,0 +1,35 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+@WebFilter(
+		urlPatterns="/*",
+		initParams={
+			@WebInitParam(name="encoding",value="UTF-8")
+		})
+public class CharacterEncodingFilter implements Filter{
+	FilterConfig config;
+	@Override
+	public void doFilter(ServletRequest request, 
+			ServletResponse response, FilterChain nextFilter)
+			throws IOException, ServletException {
+		// 서블릿을 실행하기 전에 수행할 작업
+		request.setCharacterEncoding("UTF-8");
+		nextFilter.doFilter(request, response);
+		// 서블릿을 실행한 후 수행할 작업
+		
+	}
+	@Override
+	public void init(FilterConfig config) throws ServletException{
+		this.config=config;
+	}
+
+}
